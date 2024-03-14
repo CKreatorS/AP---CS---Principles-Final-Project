1dc0ed1fd7d940d074dbdfd126fcc5944601b528
cmorales <caitlyn.morales424@gmail.com>
2024-03-14 09:19:54 -0400

    updates

--- skills/character.py
+++ skills/character.py
@@ -1,11 +1,16 @@
-from abc import abstractmethod
 
 class Character:
     """ a class defining all Characters """
-    def __init__(self, name):
+    def __init__(self, name: str, level: int, skill_trees: [], weapon: bool):
         self.name = name
+        self.level = level
+        self.skill_trees = skill_trees
+        self.weapon = weapon
+
+    def fight(self) -> str:
+        return self.name + " is at level " + self.level + " and fights with the skill trees" + self.skill_trees
+    
+class Type(Character): 
+    """ A class that identifies types of playable characters """
+    def __init__(self, name: str, level: int, skill_trees: [], weapon: str, has_weapon: bool):
         
-    @abstractmethod
-    def fight(self, skill: str):
-        """ Characters fight using skills """
-        pass
