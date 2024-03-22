3339f6f75591c0a77497f8eb83777bf31581f199
cmorales <caitlyn.morales424@gmail.com>
2024-03-22 12:35:15 -0400

    updates

--- skills/test_character.py
+++ skills/test_character.py
@@ -1,3 +1,8 @@
-from git .character import Character
-from skills.character import Features
+import random
+from skills.character import Character
+from skills.character import Features 
 from skills.character import Skill
+
+def generate_redtree () -> str:
+
+
