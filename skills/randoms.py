9081ccd48cce3596197a0b39c30480e646a7f81f
cmorales <caitlyn.morales424@gmail.com>
2024-03-25 12:13:56 -0400

    updates

---
+++ skills/randoms.py
@@ -0,0 +1,7 @@
+import random
+from skills.character import Character
+from skills.character import Features 
+from skills.character import Skill
+
+def generate_character(): 
+    
--- skills/test_character.py
+++ skills/test_character.py
@@ -1,8 +1,9 @@
-import random
 from skills.character import Character
-from skills.character import Features 
+from skills.character import Features
 from skills.character import Skill
 
-def generate_redtree () -> str:
+
+
+
 
 
