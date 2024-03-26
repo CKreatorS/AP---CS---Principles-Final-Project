bbe96cf9151840eb0b4bf4c9df131ac16df3f5fc
cmorales <caitlyn.morales424@gmail.com>
2024-03-26 12:24:47 -0400

    understanding

--- skills/character.py
+++ skills/character.py
@@ -1,33 +1,11 @@
 
 class Character:
     """ a class defining all Characters """
-    def __init__(self, name: str, level: int, skill_trees: [], weapon: bool):
+    def __init__(self, name: str, level: int, skill_trees: [], action_skill: str,  skill_points: int):
         self.name = name
         self.level = level
         self.skill_trees = skill_trees
-        self.weapon = weapon
+        self.action_skill = action_skill
+        self.skill_points = skill_points
 
-    def fight(self) -> str:
-        return self.name + " is at level " + self.level + " and fights with the skill trees" + str(self.skill_trees)
-    
-class Features(Character): 
-    """ A class that identifies the features of playable characters """
-    def __init__(self, name: str, level: int, skill_trees: [], action_skill: str, has_action_skill: bool):
-        super().__init__(name, level, skill_trees, True)
-        self.action_skill = action_skill 
-        self.has_action_skill = has_action_skill
-
-        self.has_action_skill = True
-        
-    def trees(self) -> str:
-        return 'The character ' + self.name + ' has the action skill ' + self.action_skill
-    
-class Skill(Features):
-    """ A class that identifies the action skill """
-    def __init__(self, name: str, level: int, skill_trees: [], action_skill: str, has_action_skill: bool):
-        super().__init__(name, level, skill_trees, action_skill, True)
-        self.has_action_skill = has_action_skill
-        self.has_action_skill = True
-
-    def points(self) -> str:
-        return 'it is ' + str(self.has_action_skill) + ' that ' + self.name + ' has an action skill'
+  
--- skills/randoms.py
+++
@@ -1,7 +0,0 @@
-import random
-from skills.character import Character
-from skills.character import Features 
-from skills.character import Skill
-
-def generate_character(): 
-    
